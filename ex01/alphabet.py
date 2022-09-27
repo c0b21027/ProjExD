41dd8829a78a9b1d2d0fc01e2d2eed1af57cb817
C0B21027 <c0b2102777@edu.teu.ac.jp>
2022-09-27 17:52:13 +0900

    #2

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -29,7 +29,8 @@ def kaito(seikai):
         print("正解です。それでは、具体的に欠損文字を１つづつ入力してください")
         for i in range(num):
             c = input(f"{i+1}つ目の文字を入力してください：")
-            if c not in seikai:#ここにミスがある
+            #型がおかしいので要検討
+            if c not in seikai:
                 print("不正解です。またチャレンジしてください。")
                 return False
             else:
