ea8093ae95a49402f31cb2b4643de4f845f56c7f
C0B21027 <c0b2102777@edu.teu.ac.jp>
2022-10-04 14:29:14 +0900

    02

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,4 +5,16 @@ root = tk.Tk()
 root.title("calc")
 root.geometry("300x500")
 
+r = 0
+c = 0
+
+for i,num in enumerate(range(9,-1,-1),1):
+    button = tk.Button(root, text =f"{num}",font = ("Times New Roman", 30),
+                      width= 4, height= 2)
+    button.grid(row=r,column=c)
+    c += 1
+    if i%3 == 0:
+        r += 1
+        c = 0
+
 root.mainloop()
