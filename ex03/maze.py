e2c32fcc551b5ee86e44a1213c9828b700baf6e7
C0B21027 <c0b2102777@edu.teu.ac.jp>
2022-10-18 15:33:27 +0900

    rennsyuu

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,15 +10,27 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx,cy
+    global mx, my
+    global cx, cy
     if key =="Up":
-        cy -=20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    if maze_list[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key =="Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
     canv.coords("tori", cx, cy)
     root.after(100,main_proc)
 
@@ -30,8 +42,14 @@ if __name__ == "__main__":
     canv = tk.Canvas(root,width = 1500,height = 900,bg ="black")
     canv.pack()
 
+    maze_list = mm.make_maze(15,9)
+    print(maze_list)#1壁,0床
+
+    mm.show_maze(canv, maze_list)
+
     tori = tk.PhotoImage(file = "fig/8.png")
-    cx,cy = 300,400
+    mx,my = 1,1
+    cx,cy = mx*300+50,my*400+50
     canv.create_image(cx,cy,image=tori,tag="tori")
 
     key = ""
@@ -40,10 +58,5 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>",key_up)
 
     main_proc()
-
-    maze_list = mm.make_maze(15,9)
-    print(maze_list)#1壁,0床
-
-    mm.show_maze(canv, maze_list)
 
     root.mainloop()
